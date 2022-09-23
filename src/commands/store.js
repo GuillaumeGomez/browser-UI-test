2a1d849361d99bc5bc3675980d6bc2da8cb35732
Guillaume Gomez <guillaume1.gomez@gmail.com>
2022-09-23 17:57:21 +0200

    Add store-text command

--- src/commands/store.js
+++ src/commands/store.js
@@ -265,10 +265,70 @@ arg.variables["${tuple[0].getText()}"] = await jsHandle.jsonValue();`,
     };
 }
 
+
+// Possible inputs:
+//
+// * (ident, "CSS selector" | "XPath"
+function parseStoreText(parser) {
+    const elems = parser.elems;
+
+    if (elems.length === 0) {
+        return {'error': 'expected a tuple, found nothing'};
+    } else if (elems.length !== 1 || elems[0].kind !== 'tuple') {
+        return {
+            'error': `expected a tuple, found \`${parser.getRawArgs()}\``,
+        };
+    }
+    const tuple = elems[0].getRaw();
+    if (tuple.length !== 2) {
+        let err = `expected 2 elements in the tuple, found ${tuple.length} element`;
+        if (tuple.length > 1) {
+            err += 's';
+        }
+        return {'error': err};
+    } else if (tuple[0].kind !== 'ident') {
+        return {
+            'error': `expected first argument to be an ident, found ${tuple[0].getArticleKind()} \
+(\`${tuple[0].getText()}\`)`,
+        };
+    } else if (tuple[1].kind !== 'string') {
+        return {
+            'error': `expected second argument to be a CSS selector or an XPath, found \
+${tuple[1].getArticleKind()} (\`${tuple[1].getText()}\`)`,
+        };
+    }
+
+    const selector = tuple[1].getSelector();
+    if (selector.error !== undefined) {
+        return selector;
+    }
+    const warnings = [];
+    const isPseudo = !selector.isXPath && selector.pseudo !== null;
+    if (isPseudo) {
+        warnings.push(`Pseudo-elements (\`${selector.pseudo}\`) don't have attributes so \
+the check will be performed on the element itself`);
+    }
+
+    const varName = 'parseStoreText';
+    const code = `\
+${getAndSetElements(selector, varName, false)}
+const jsHandle = await ${varName}.evaluateHandle(e => {
+    return browserUiTestHelpers.getElemText(e, "");
+});
+arg.variables["${tuple[0].getText()}"] = await jsHandle.jsonValue();`;
+
+    return {
+        'instructions': [code],
+        'wait': false,
+        'warnings': warnings.length !== 0 ? warnings : undefined,
+    };
+}
+
 module.exports = {
     'parseStoreAttribute': parseStoreAttribute,
     'parseStoreCss': parseStoreCss,
     'parseStoreLocalStorage': parseStoreLocalStorage,
     'parseStoreProperty': parseStoreProperty,
+    'parseStoreText': parseStoreText,
     'parseStoreValue': parseStoreValue,
 };
